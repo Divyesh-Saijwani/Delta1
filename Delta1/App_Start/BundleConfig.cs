ee075d9a7abd9f634e997a2c3f38780fdf9c50b4
Divyesh Saijwani <Divyesh Saijwani>
2017-04-09 17:51:08 +0530

    New Start With Areas

--- Delta1/App_Start/BundleConfig.cs
+++ Delta1/App_Start/BundleConfig.cs
@@ -9,38 +9,23 @@ namespace Delta1
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/js/jquery.min.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                        "~/Scripts/js/detect.js",
-                        "~/Scripts/js/fastclick.js",
-                        "~/Scripts/js/jquery.blockUI.js",
-                        "~/Scripts/js/waves.js",
-                        "~/Scripts/js/jquery.slimscroll.js",
-                        "~/Scripts/js/jquery.scrollTo.min.js"));
-            bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                        "~/Scripts/js/jquery.core.js",
-                        "~/Scripts/js/jquery.app.js"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/components.css",
-                      "~/Content/icons.css",
-                      "~/Content/menu.css",
-                      "~/Content/pages.css",
-                      "~/Content/responsive.css",
-                      "~/Content/core.css"));
-
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
         }
     }
 }
